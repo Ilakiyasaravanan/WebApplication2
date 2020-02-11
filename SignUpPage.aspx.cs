8d5023609e1240d2b8fdbc2e40c1d1ea62fb6b44
ILAKIYA S <ilakyaengi@outlook.com>
2020-02-11 09:55:08 +0530

    Try

--- SignUpPage.aspx.cs
+++ SignUpPage.aspx.cs
@@ -7,55 +7,13 @@ namespace JobPotal_Entity
 {
 	public partial class SignUpPage : System.Web.UI.Page
 	{
-		string passWord, role, firstName, lastName, address, gender, email;
-		long phone;
+		
 		protected void Page_Load(object sender, EventArgs e)
 		{
 
 		}
 		
 
-		protected void SignUp_Click(object sender, EventArgs e)
-		{
-
-			firstName = txtFirstname.Text;
-			lastName = txtLastName.Text;
-			address = txtAddress.Text;
-			passWord = txtPassword.Text;
-			gender = rdGender.Text;
-			email = txtEmail.Text;
-			role = rdUser.Text;
-			phone = long.Parse(txtPhonenumber.Text);
-			GenerateAccount();
-		}
-
-		public void GenerateAccount()
-		{
-			string temp = email + firstName;
-			long hashcode = Math.Abs(temp.GetHashCode());
-			string verify = new RepositoryMediator().AccountCheck(hashcode);
-			if (verify.Equals("Job Searcher") || verify.Equals("Recruiter"))
-			{
-				Response.Write("Retry with another name and email id");
-			}
-			else
-			{
-				PersonDetails person = new PersonDetails(firstName, phone, lastName, address, passWord, gender, role, email, hashcode);
-				
-			    int result= new RepositoryMediator().CreateAccount(person);
-				if (result == 1)
-				{
-
-					Response.Write("signin successfully!!! Your account number is: " + hashcode);
-				}
-				else
-				{
-					Response.Write("Unable to create account");
-				}
-				
-			}
-		}
-
-	}
+		
 
 }
